fa5dd691bad93200ad965ed24b083021c3a26e17
marytriza <107030787+marytriza@users.noreply.github.com>
2022-09-29 04:42:27 +0300

    delete 0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    number_of_elements = len(argv)
-    print("{:d} {:s}{:s}".format(number_of_elements - 1, "argument" if number_of_elements <= 2 else "arguments","." if number_of_elements == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
